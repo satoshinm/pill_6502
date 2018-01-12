efb8da7c7a08083b925bdb2f96efcc9b845c54a0
Satoshi N. M <snmatsutake@yahoo.co.jp>
2018-01-11 21:07:54 -0800

    fake6502: comment out unused static function getvalue16()
    Unused and causes compiler warning/error
    https://stackoverflow.com/questions/11124895/suppress-compiler-warning-function-declared-never-referenced

--- src/fake6502.c
+++ src/fake6502.c
@@ -307,9 +307,11 @@ static uint16_t getvalue() {
         else return((uint16_t)read6502(ea));
 }
 
+/* unused
 static uint16_t getvalue16() {
     return((uint16_t)read6502(ea) | ((uint16_t)read6502(ea+1) << 8));
 }
+*/
 
 static void putvalue(uint16_t saveval) {
     if (addrtable[opcode] == acc) a = (uint8_t)(saveval & 0x00FF);
